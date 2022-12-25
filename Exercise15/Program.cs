764a27486653e652238360e9e7e01bac75b34853
Irina Gorynina <iren.gorynina@gmail.com>
2022-12-25 23:42:30 +0300

    Написали программу для 15 задачи

---
+++ Exercise15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+int number = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(number);
+int a = number.ToString().Length;
+if (a > 0 && a < 8)
+{
+    if (a == 7 || a == 6)
+    {
+        Console.Write("Выходной");
+    }
+    else
+    {
+        Console.Write("Рабочий");
+    }
+}
+else
+{
+    Console.Write("Невозможно определить день недели");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
